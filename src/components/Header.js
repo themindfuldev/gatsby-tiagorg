a43e1271c08f6b2973bf7c291102a5fa32dc3f27
Tiago Garcia <tiagoromerogarcia@gmail.com>
2018-08-27 20:36:16 -0700

    changing name

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,9 @@ class Header extends React.Component {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <a href="#" className="image avatar"><img src="https://s.gravatar.com/avatar/5cac784a074b86d771fe768274f6860c?s=600" alt="Tiago Romero Garcia" /></a>
+                    <h1>Hi. I am <strong>Tiago Romero Garcia</strong>.</h1>
+                    <p>Front-end architect in San Francisco. Conference speaker and article writer. Believer in servant leadership and ahimsa. Focusing on equanimity through mindfulness. Platelets donor.</p>
                 </div>
                 <Footer />
             </header>
