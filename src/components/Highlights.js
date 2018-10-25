6e64d65a6743f1dd68e109c6845c346b25a337b1
Tiago Garcia <tiagoromerogarcia@gmail.com>
2018-10-24 19:39:36 -0700

    highlights simplification

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -14,24 +14,22 @@ class Highlights extends React.Component {
                         Jasmine, Java, Git, Webpack, Unit Tests, Functional Programming, Lazy Loading,
                         Design Patterns, Responsive Design.</li>         
                     <li>Architected, orchestrated and spearheaded the implementation of several pages and features
-                        for top U.S. e-commerces, by leading teams and performing redesigns and modernizations on 
-                        their architecture, tech stacks and user experience, always favoring performance, team productivity, 
-                        componentization, responsive design, design patterns, unit tests, SEO and accessibility.</li>
+                        for top U.S. e-commerces. I led teams and revamped the architecture, tech stacks and user experience, 
+                        targeting productivity, performance, 
+                        responsive design, design patterns, unit tests, SEO and accessibility.</li>
                     <li>Empowered companies to achieve admirable results in terms of increased revenue, reduction
-                        in page load time and speed index, improved user engagement, boosted SEO ranking
-                        while enhancing the team's productivity by adopting modern technologies, coaching and 
-                        teaching innovative techniques.</li>
+                        in page load time and speed index, improved user engagement and boosted SEO ranking. I
+                        enhanced team productivity by adopting modern technologies,  
+                        teaching innovative techniques and coaching.</li>
                     <li>Started <ExternalLink href="https://avenuecode.com">Avenue Code</ExternalLink>'s
                         branch in Brazil as its first employee, assuming the roles of Software Engineer,
                         Tech Lead and Tech Manager. In the first 4 years,
                         I led the growth to 120 employees in 3 offices.</li>
                     <li>Awarded <ExternalLink href="https://www.airpair.com/100k-writing-competition">First 
-                        Prize on AirPair's 2015 Developer Writing Competition</ExternalLink> with my article <ExternalLink
-                        href="https://www.airpair.com/javascript/posts/the-mind-boggling-universe-of-javascript-modules">
-                        The mind-boggling universe of JavaScript Module strategies</ExternalLink>.</li>
+                        Prize on AirPair's 2015 Developer Writing Competition</ExternalLink>.</li>
                     <li>Awarded First Place among the Computer Science students (equivalent to <i>summa cum laude</i>).</li>
                     <li>Organized the <ExternalLink href="https://www.meetup.com/Backbone-js-Hackers/">
-                        Backbone.js Hackers meetup</ExternalLink> in San Francisco, with 1000 members at the time.</li>
+                        Backbone.js Hackers meetup</ExternalLink> in San Francisco from 2014 to 2016, having 1000 members.</li>
                 </ul>
                 <ul className="actions">
                     <li>
